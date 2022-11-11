0a2deafd0ef83545f0805d583a3a4e19b1ab0343
zakumann <samuel742able@gmail.com>
2022-11-11 16:38:27 +0900

    upload Code_Tetris and GameDev

---
+++ Code_Tetris/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#start-button')
+    const width = 10
+})
---
+++ GameDev/Project/hello.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+/*
+hello.cpp
+prints out my name, home twon,
+and my favorite hobby.
+by JK Lee.
+*/
+int main()
+{
+    //name
+    printf("Hello, My name is JungKwan Lee.\n");
+    //hometown
+    printf("I live in Dalsung gun, Daegu(Taegu).\n");
+    //Hobby
+    printf("I enjoy Reading books and Computer Coding.");
+}
