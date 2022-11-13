6eb67f2090948776f568588ed241adf25aeb3fab
zakumann <samuel742able@gmail.com>
2022-11-13 14:05:40 +0900

    Tetris add taken class

---
+++ Code_Tetris/README.md
@@ -0,0 +1,5 @@
+Tetris, Clon Code
+
+reference : https://www.youtube.com/watch?v=rAUn1Lom6dw
+
+copyright&copy; Tetris Company
--- Code_Tetris/app.js
+++ Code_Tetris/app.js
@@ -1,7 +1,90 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     let squares = Array.from(document.querySelectorAll('.grid div'))
-    const ScoreDisplay = document.querySelector('#score')
-    const StartBtn = document.querySelector('#start-button')
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
     const width = 10
+
+    //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+  let currentPosition = 4
+  let currentRotation = 0
+
+  console.log(theTetrominoes[0][0])
+  //randomly select a Tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[0][0]
+
+  //draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+  //undraw the Tetromino
+  function undraw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+  //make the tetromino move down every second
+  timerId= setInterval(moveDown, 1000)
+
+  //move down function
+  function moveDown(){
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+  }
+
+  //freeze function
+  function freeze(){
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+      current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      //start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length)
+      current = theTetrominoes[random][currentRotation]
+      currentPosition = 4
+      draw()
+    }
+  }
+
 })
