94b0b7ce8a3dd4b80c1a59bf7584c33554f41da5
zakumann <samuel742able@gmail.com>
2022-11-14 12:55:55 +0900

    add rotate and move

--- Code_Tetris/app.js
+++ Code_Tetris/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scoreDisplay = document.querySelector('#score')
     const startBtn = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
 
     //The Tetrominoes
   const lTetromino = [
@@ -49,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log(theTetrominoes[0][0])
   //randomly select a Tetromino and its first rotation
   let random = Math.floor(Math.random()*theTetrominoes.length)
-  let current = theTetrominoes[0][0]
+  let current = theTetrominoes[random][currentRotation]
 
   //draw the Tetromino
   function draw() {
@@ -67,6 +68,20 @@ document.addEventListener('DOMContentLoaded', () => {
   //make the tetromino move down every second
   timerId= setInterval(moveDown, 1000)
 
+  //assign functions to keyCodes
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    } else if (e.keyCode === 38) {
+      rotate()
+    } else if (e.keyCode === 39) {
+      moveRight()
+    } else if (e.keyCode === 40) {
+      moveDown()
+    }
+  }
+  document.addEventListener('keyup', control)
+
   //move down function
   function moveDown(){
     undraw()
@@ -76,15 +91,81 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //freeze function
-  function freeze(){
-    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+  function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
       //start a new tetromino falling
-      random = Math.floor(Math.random() * theTetrominoes.length)
+      random = nextRandom
+      nextRandom = Math.floor(Math.random() * theTetrominoes.length)
       current = theTetrominoes[random][currentRotation]
       currentPosition = 4
       draw()
+      displayShape()
     }
+  }
+
+  //move the tetromino left, unless is at the edge or there is a blockage
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+    if(!isAtLeftEdge) currentPosition -=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition +=1
+    }
+
+    draw()
+  }
+
+  //move the tetromino right, unless is at the edge or there is a blockage
+  function moveRight(){
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+    if(!isAtRightEdge) currentPosition +=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -=1
+    }
+
+    draw()
+  }
+
+  //rotate the tetromino
+  function rotate(){
+    undraw()
+    currentRotation ++
+    if(currentRotation === current.length){ //if the current rotation gets to 4, make it go back to 0
+      currentRotation = 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+  }
+
+  //show up-next tetromino in mini-grid
+  const displaySquares = document.querySelectorAll('.mini-grid div')
+  const displayWidth = 4
+  let displayIndex = 0
+
+  //the Tetrominos without rotations
+  const upNextTetrominoes = [
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
+    [0, 1, displayWidth, displayWidth+1], // oTetromino
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
+  ]
+
+  //display the shape in the mini-grid display
+  function displayShape(){
+    //remove any trace of a tetromino form the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+    })
+    upNextTetrominoes[nextRandom].forEach( index => {
+      displaySquares[displayIndex + index].classList.add('tetromino')
+    })
   }
 
 })
--- GameDev/Axe_game/axe_game.cpp
+++ GameDev/Axe_game/axe_game.cpp
@@ -14,47 +14,78 @@ int main()
     int l_circle_x{circle_x - circle_radius};
     int r_circle_x{circle_x + circle_radius};
     int u_circle_y{circle_y - circle_radius};
-    int d_circle_y{circle_y + circle_radius};
+    int b_circle_y{circle_y + circle_radius};
 
     // axe coordinates
     int axe_x{400};
     int axe_y{0};
     int axe_length{50};
+
     // axe edges
     int l_axe_x{axe_x};
     int r_axe_x{axe_x + axe_length};
     int u_axe_y{axe_y};
-    int d_axe_y{axe_y + axe_length};
+    int b_axe_y{axe_y + axe_length};
 
     int direction(10);
 
+    bool collision_with_axe = 
+                (b_axe_y >= u_circle_y) && 
+                (u_axe_y <= b_circle_y) && 
+                (r_axe_x >= l_circle_x) && 
+                (l_axe_x <= r_circle_x);
+
     SetTargetFPS(60);
-    while(WindowShouldClose() != true)
+    while(WindowShouldClose() == false)
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        // Game logic begins
-
-        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
-        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
-
-        // move the axe
-        axe_y += direction;
-        if(axe_y > height || axe_y < 0)
+        if (collision_with_axe)
         {
-            direction = -direction;
+            DrawText("Game Over!", 400, 200, 20, RED);
+        }
+        else
+        {
+            // Game logic begins
+
+            //update the edges
+            l_circle_x = circle_x - circle_radius;
+            r_circle_x = circle_x + circle_radius;
+            u_circle_y = circle_y - circle_radius;
+            b_circle_y = circle_y + circle_radius;
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
+            // update collision_with_axe
+            collision_with_axe = 
+                (b_axe_y >= u_circle_y) && 
+                (u_axe_y <= b_circle_y) && 
+                (r_axe_x >= l_circle_x) && 
+                (l_axe_x <= r_circle_x);
+
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+            DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
+            // move the axe
+            axe_y += direction;
+            if(axe_y > height || axe_y < 0)
+            {
+                direction = -direction;
+            }
+
+            if (IsKeyDown(KEY_D) && circle_x < width)
+            {
+                circle_x += 10;
+            }
+            if (IsKeyDown(KEY_A) && circle_x > 0)
+            {
+                circle_x -= 10;
+            }
         }
 
-        if (IsKeyDown(KEY_D) && circle_x < width)
-        {
-            circle_x += 10;
-        }
-        if (IsKeyDown(KEY_A) && circle_x > 0)
-        {
-            circle_x -= 10;
-        }
-        
+        //Game logic ends
         EndDrawing();
     }
 }
